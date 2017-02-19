12d9a38be56f1b3d3db5d86e70b3d8501739745d
Peter Trizuliak <peter.trizuliak@gmail.com>
2017-02-19 08:02:32 +0000

    Add canvas background and Ash can move

--- public/index.js
+++ public/index.js
@@ -1,46 +1,106 @@
-/*
-
-https://img.pokemondb.net/sprites/red-blue/normal/wynaut.png
-
-*/
-
-var makeRequest = function(url, callback) {
-  var request = new XMLHttpRequest();
-  request.open("GET", url);
-  request.onload = callback;
-  request.send();
-};
-
-var requestComplete = function() {
-  console.log("We made a request");
-  if( this.status !== 200) return;
-  var jsonString = this.responseText;
-  var pokemons = JSON.parse(jsonString);
-  console.log('this is pokemons:', pokemons);
-  console.log('results:', pokemons.pokemon);
-  console.log('results:', pokemons.pokemon[0]);
-  console.log('results:', pokemons.pokemon[0].name);
-  populateList(pokemons.pokemon);
-};
-
-
-var populateList = function(APIarray) {
-  var ul = document.getElementById('country-list');
-  for (var i = 0; i < APIarray.length; i++) {
-    var li = document.createElement('li');
-    li.innerText = APIarray[i].name;
-    ul.appendChild(li);
-  };
-};
-
-var onButtonClick = function() {
-  var url = "https://pokeapi.co/api/v1/pokedex/1/";
-  makeRequest(url, requestComplete);
-};
-
 var app = function(){
-  var button = document.querySelector('#populate-button');
-  button.onclick = onButtonClick;
+
+  var pokemons = ["pikachu", "raichu", "charmander"];
+  var player = [];
+  var field = [];
+  var ash = document.createElement('img');
+  ash.src = "img/ash.png";
+  var grass = document.createElement('img');
+  grass.src = "img/grass.png";
+  var x = 300;
+  var y = 200;
+  var canvas = document.querySelector("#island");
+  var context = canvas.getContext('2d');
+
+  /*
+  https://img.pokemondb.net/sprites/red-blue/normal/wynaut.png
+  */
+
+  ///////////// CREATION OF POKEMON LIST //////////////////////////
+
+  var requestPokemon = function(callback) {
+    console.log('requesting Pokemon');
+    var request = new XMLHttpRequest();
+    var link = 'https://pokeapi.co/api/v2/pokedex/2/';
+    request.open("GET", link);
+    request.onload = callback;
+    request.send();
+  };
+
+  var completePokemon = function() {
+    if (this.status !== 200) return;
+    var jsonString = this.responseText;
+    var jsonObject = JSON.parse(jsonString);
+    var pokemonsObjects = jsonObject.pokemon_entries;
+    for (var i = 0; i < pokemonsObjects.length; i ++) {
+      pokemons.push(pokemonsObjects[i].pokemon_species.name);
+    }
+    console.log('completing Pokemon');
+  };
+
+  var populatePokemonSelect = function() {
+    var pokeSelection = document.getElementById('pokemon-select')
+    for (var each of pokemons) {
+      var pokeChoice = document.createElement('option');
+      pokeChoice.innerText = each;
+      pokeSelection.appendChild(pokeChoice);
+    }
+  };
+
+  ///////////// FIELD GRAPHIC SETUP ////////////////////////////////////
+
+  var drawAsh = function() {
+    context.drawImage(grass, 0, 0, 335, 166);
+    context.drawImage(grass, 335, 0, 335, 166);
+    context.drawImage(grass, 0, 166, 335, 166);
+    context.drawImage(grass, 335, 166, 335, 166);
+    context.drawImage(grass, 0, 332, 335, 166);
+    context.drawImage(grass, 335, 332, 335, 166);
+    context.drawImage(ash, x - 20, y - 20, 40, 40);
+  };
+
+  var moveAsh = function(xIncrement, yIncrement) {
+    context.drawImage(grass, 0, 0, 335, 166);
+    context.drawImage(grass, 335, 0, 335, 166);
+    context.drawImage(grass, 0, 166, 335, 166);
+    context.drawImage(grass, 335, 166, 335, 166);
+    context.drawImage(grass, 0, 332, 335, 166);
+    context.drawImage(grass, 335, 332, 335, 166);
+    context.drawImage(ash, x -20 + xIncrement, y -20 + yIncrement, 40, 40);
+    x += xIncrement;
+    y += yIncrement;
+  };
+
+  document.onkeydown = function(event) {
+    var increment = 10;
+    console.log(event.keyCode);
+    if (event.keyCode === 80) {
+      drawAsh();
+    }
+    if (event.keyCode === 39) {
+      // right
+      moveAsh(increment, 0);
+    }
+    if (event.keyCode === 37) {
+      // left
+      moveAsh(-increment, 0);
+    }
+    if (event.keyCode === 38) {
+      // up
+      moveAsh(0, -increment);
+    }
+    if (event.keyCode === 40) {
+      // down
+      moveAsh(0, increment);
+    }
+  };
+
+  // requestPokemon(completePokemon);
+  populatePokemonSelect();
+  console.log('pokemons', pokemons);
+  drawAsh();
+
+
 };
 
 window.onload = app;
