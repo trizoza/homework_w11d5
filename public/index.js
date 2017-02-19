8cd91cf5ff501ca357b94a7a12f75d1b6d0bbf57
Peter Trizuliak <peter.trizuliak@gmail.com>
2017-02-19 20:54:59 +0000

    Add sand frame

--- public/index.js
+++ public/index.js
@@ -2,7 +2,6 @@ var app = function(){
 
   var pokemons = [];
   var player = [];
-  var field = [];
   var ash = document.createElement('img');
   ash.src = "img/ash.png";
   var ashWidth = 40;
@@ -27,6 +26,11 @@ var app = function(){
   var y3 = 0;
   var poke3 = {};
   var poke3pic = document.createElement('img');
+  var pics = [pokepic, poke1pic, poke2pic, poke3pic];
+  var winner = "tie";
+  var oak = document.createElement('img');
+  oak.src = "img/oak.png";
+  oak.setAttribute("id", "oak");
 
   ///////////// CREATION OF POKEMON LIST //////////////////////////
 
@@ -47,8 +51,10 @@ var app = function(){
     for (var i = 0; i < pokemonsObjects.length; i ++) {
       var eachPokemon = {
         name: "",
-        attack: 0
+        attack: 0,
+        src: ""
       };
+      eachPokemon.src = "https://img.pokemondb.net/sprites/red-blue/normal/" + pokemonsObjects[i].pokemon_species.name + ".png"
       eachPokemon.name = pokemonsObjects[i].pokemon_species.name;
       eachPokemon.attack = getRandomInt(30, 100);
       eachPokemon.name = eachPokemon.name.substring(0, 1).toUpperCase() + eachPokemon.name.substring(1);
@@ -76,11 +82,9 @@ var app = function(){
       }
     }
     console.log(player[0]);
-    pokepic.src = "https://img.pokemondb.net/sprites/red-blue/normal/" + player[0].name.toLowerCase() + ".png";
+    pokepic.src = player[0].src;
   }
-
   var select = document.querySelector('#pokemon-select');
-  
   select.onchange = handleSelectPokemon;
   
 
@@ -115,6 +119,7 @@ var app = function(){
       var questionText = document.createElement('p'); 
       questionText.innerHTML = "<p>Do you want to <u>f</u>ight him?</p>";
       foundScreen.appendChild(foundText);
+      foundScreen.appendChild(poke1pic);
       foundScreen.appendChild(questionText);
       document.querySelector("#island").classList.toggle("show");
       document.querySelector("#found").classList.toggle("show");
@@ -127,6 +132,7 @@ var app = function(){
       var questionText = document.createElement('p'); 
       questionText.innerHTML = "<p>Do you want to <u>f</u>ight him?</p>";
       foundScreen.appendChild(foundText);
+      foundScreen.appendChild(poke2pic);
       foundScreen.appendChild(questionText);
       document.querySelector("#island").classList.toggle("show");
       document.querySelector("#found").classList.toggle("show");
@@ -139,6 +145,7 @@ var app = function(){
       var questionText = document.createElement('p'); 
       questionText.innerHTML = "<p>Do you want to <u>f</u>ight him?</p>";
       foundScreen.appendChild(foundText);
+      foundScreen.appendChild(poke3pic);
       foundScreen.appendChild(questionText);
       document.querySelector("#island").classList.toggle("show");
       document.querySelector("#found").classList.toggle("show");
@@ -182,22 +189,23 @@ var app = function(){
     x1 = getRandomInt(2/2, 58/2)*increment;
     y1 = getRandomInt(2/2, 38/2)*increment;
     poke1 = pokemons[getRandomInt(0, pokemons.length)];
-    poke1pic.src = "https://img.pokemondb.net/sprites/red-blue/normal/" + poke1.name.toLowerCase() + ".png";
+    poke1pic.src = poke1.src;
     console.log('first free pokemon',x1, y1, poke1.name);
     x2 = getRandomInt(2/2, 58/2)*increment;
     y2 = getRandomInt(2/2, 38/2)*increment;
     poke2 = pokemons[getRandomInt(0, pokemons.length)];
-    poke2pic.src = "https://img.pokemondb.net/sprites/red-blue/normal/" + poke2.name.toLowerCase() + ".png";
+    poke2pic.src = poke2.src;
     console.log('second free pokemon',x2, y2, poke2.name);
     x3 = getRandomInt(2/2, 58/2)*increment;
     y3 = getRandomInt(2/2, 38/2)*increment;
     poke3 = pokemons[getRandomInt(0, pokemons.length)];
-    poke3pic.src = "https://img.pokemondb.net/sprites/red-blue/normal/" + poke3.name.toLowerCase() + ".png";
+    poke3pic.src = poke3.src;
     console.log('third free pokemon',x3, y3, poke3.name);
   };
 
   document.onkeydown = function(event) {
     console.log(event.keyCode);
+
     if (event.keyCode === 80 && player.length != 0) {
       // p
       document.querySelector("#menu").classList.toggle("hide");
@@ -207,6 +215,7 @@ var app = function(){
       generateFreePokemons();
       drawAsh();
     }
+
     if (event.keyCode === 39) {
       // right
       if (x === x1 && y === y1) {
@@ -228,6 +237,7 @@ var app = function(){
         moveAsh(increment, 0);
       }
     }
+
     if (event.keyCode === 37) {
       // left
       if (x === x1 && y === y1) {
@@ -252,6 +262,7 @@ var app = function(){
         moveAsh(-increment, 0);
       }
     }
+
     if (event.keyCode === 38) {
       // up
       if (x === x1 && y === y1) {
@@ -273,6 +284,7 @@ var app = function(){
         moveAsh(0, -increment);        
       }
     }
+
     if (event.keyCode === 40) {
       // down
       if (x === x1 && y === y1) {
@@ -294,34 +306,129 @@ var app = function(){
         moveAsh(0, increment);
       }
     }
+
     ///////// fight ///////////////////
     if (event.keyCode === 70) {
       // f
       if (x === x1 && y === y1) {
+        calculateWinner(poke1);
+        winLose(poke1);
         document.querySelector("#fight").classList.toggle("show");
         document.querySelector("#found").classList.toggle("show");
       }
       else if (x === x2 && y === y2) {
+        calculateWinner(poke2);
+        winLose(poke2);
         document.querySelector("#fight").classList.toggle("show");
         document.querySelector("#found").classList.toggle("show");
       }
       else if (x === x3 && y === y3) {
+        calculateWinner(poke3);
+        winLose(poke3);
         document.querySelector("#fight").classList.toggle("show");
         document.querySelector("#found").classList.toggle("show");
       }
     }
+    //////// continue ////////////////////////////
+    if (event.keyCode === 67) {
+      // c
+      document.querySelector("#fight").classList.toggle("show");
+      document.querySelector("#island").classList.toggle("show");
+    }
+    /////// reload ///////////////////////////////
+    if (event.keyCode === 65) {
+      //a
+      window.location.reload(true);
+    }
   };
 
-  var calculateWinner = function(powerNumber) {
-    if (power > powerNumber) {
-
+  var calculateWinner = function(foundpokemon) {
+    console.log("unsorted", player);
+    player.sort(function(a,b) {
+      return b.attack - a.attack;
+    });
+    console.log('sorted', player);
+    if (player[0].attack > foundpokemon.attack || player[0].attack === foundpokemon.attack) {
+      winner = true;
     }
+    else {
+      winner = false;
+    }
+    return winner;
+    console.log(winner)
+  };
+
+  var winLose = function(foughtPokemon) {
+    var fightScreen = document.querySelector('#fight');
+    var wonMessage = document.createElement('p');
+    var lostMessage = document.createElement('p');
+    var totalLost = document.createElement('p');
+    fightScreen.innerHTML = "";
+    if (winner) {
+      player.push(foughtPokemon);
+      
+      if (foughtPokemon === poke1) {
+        x1 = 1000;
+        y1 = 1000;
+      }
+      if (foughtPokemon === poke2) {
+        x2 = 1000;
+        y2 = 1000;
+      }
+      if (foughtPokemon === poke3) {
+        x3 = 1000;
+        y3 = 1000;
+      }
+      
+      console.log('you won');
+      wonMessage.innerText = "Your " + player[0].name 
+      + " with attack " + player[0].attack 
+      + " won over " + foughtPokemon.name
+      + " with attack " + foughtPokemon.attack
+      + " therefore " + foughtPokemon.name 
+      + " is now yours! Yeeey, you have now these Pokémon:";
+      fightScreen.appendChild(wonMessage);
+      for (var eachPokemon of player) {
+        for (var eachPic of pics) {
+          if (eachPokemon.src === eachPic.src) {
+            fightScreen.appendChild(eachPic);
+          }
+        }
+      }
+      //////// end of game check ////////////////
+      if (player.length === 4) {
+        var totalWin = document.createElement('p');
+        totalWin.innerHTML = "<p>Awesome! You have caught them all! Wanna play <u>a</u>gain?<p>";
+        fightScreen.appendChild(totalWin);
+        fightScreen.appendChild(oak);
+      }
+      else {
+        var semiWin = document.createElement('p');
+        semiWin.innerHTML = "<p><u>C</u>ontinue catching them?<p>";
+        fightScreen.appendChild(semiWin);
+      }
+    }
+
+    else {
+      console.log('you lost');
+      lostMessage.innerText = "Your strongest Pokémon " + player[0].name 
+      + " with attack " + player[0].attack 
+      + " lost with " + foughtPokemon.name
+      + " with attack " + foughtPokemon.attack
+      + "! Ooops, game is over!";
+      fightScreen.appendChild(lostMessage);
+      totalLost.innerHTML = "<p>Wanna play <u>a</u>gain?<p>";
+      fightScreen.appendChild(totalLost);
+    }
+  };
+
+  var reload = function() {
+    window.reload;
   };
 
   requestPokemon(completePokemon);
   console.log('pokemons', pokemons);
   drawAsh();
-
 
 };
 
